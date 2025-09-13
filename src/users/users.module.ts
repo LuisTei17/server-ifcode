50f026866511df59a391ba12f597e7cdc4482d20
luis.teixeira <luisedteixeira@gmail.com>
2025-09-13 08:00:01 -0300

    Adding unit tests

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,14 +3,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Interest } from './interest.entity';
 import { UsuarioInteresse } from './usuario-interesse.entity';
+import { TipoUsuario } from './tipo-usuario.entity';
 import { UsersService } from './users.service';
 import { UsuarioInteresseService } from './usuario-interesse.service';
 import { InterestController } from './interest.controller';
+import { TipoUsuarioController } from './tipo-usuario.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Interest, UsuarioInteresse])],
+  imports: [TypeOrmModule.forFeature([User, Interest, UsuarioInteresse, TipoUsuario])],
   providers: [UsersService, UsuarioInteresseService],
-  controllers: [InterestController],
+  controllers: [InterestController, TipoUsuarioController],
   exports: [UsersService, UsuarioInteresseService],
 })
 export class UsersModule {}
